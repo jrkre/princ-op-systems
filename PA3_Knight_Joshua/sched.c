c6480be11bf8e4dc92c110e8dc17b0f48f394a87
Joshua Knight <joshuaknight@nevada.unr.edu>
2025-03-10 14:56:11 -0700

    progress

--- PA3_Knight_Joshua/sched.c
+++ PA3_Knight_Joshua/sched.c
@@ -1,8 +1,17 @@
+#include <bits/time.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <pthread.h>
+#include <bits/time.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/mman.h>
+#include "print_progress.c"
+
+
 
 typedef struct _thread_data_t { 
     int localTid;
@@ -11,6 +20,7 @@ typedef struct _thread_data_t {
     pthread_mutex_t *lock;
     long long int *totalSum;
 } thread_data_t;
+
 
 
 void* arraySum(void* void_data)
@@ -23,20 +33,35 @@ void* arraySum(void* void_data)
 
     while(1)
     {
+        long latency_max = 0;
         for (int i = 0; i < data->numVals; i++)
         {
-            threadSum += data->data[i];
-        }
+            struct timespec start, end;
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            long latency = 0;
 
+            threadSum += data->data[i];
+            
+            clock_gettime(CLOCK_MONOTONIC, &end);
+            latency = (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
+            if (latency > latency_max)
+            {
+                latency_max = latency;
+            }
+        }
+        
         // printf("%lld start: %d end: %d\n", threadSum, data->startInd, data->endInd);
         pthread_mutex_lock(data->lock);
         *data->totalSum += threadSum;
         pthread_mutex_unlock(data->lock);
+        
+        print_progress(data->localTid, latency_max);
     }
     
 
     return data->totalSum;
 }
+
 
 
 int main(int argc, char* argv[])
@@ -56,7 +81,8 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    int thread_count = argv[1];
+    int thread_count = atoi(argv[1]);
+
     printf("%s%d\n", "Thread Count: ", thread_count);
 
     int numVals = 2000000;
@@ -78,9 +104,26 @@ int main(int argc, char* argv[])
         thread_array[i].totalSum = &totalSum;
     }
 
+    pthread_t * threads = malloc(thread_count * sizeof(pthread_t));
+
+    for (int i = 0; i < thread_count; i++)
+    {
+        thread_array[i].numVals = numVals;
+        pthread_create(&threads[i], NULL, arraySum, &thread_array[i]);
+    }
+
+    for (int i = 0; i < thread_count; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
     
+    pthread_mutex_destroy(&mutex);
 
+    free(vals);
+    free(thread_array);
+    free(threads);
 
-
-
+    return 0;
 };
+
+
